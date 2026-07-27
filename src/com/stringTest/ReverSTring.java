5a717b32af7a80d641db8830105d50463a2005b2
sachnsharma0607 <sachnsharma0607@gmail.com>
2026-07-27 11:51:43 +0530

    implement the revstr method (#2)
    Co-authored-by: sachin <sachinorsharma@gmail.com>

--- src/com/stringTest/ReverSTring.java
+++ src/com/stringTest/ReverSTring.java
@@ -3,10 +3,19 @@ package com.stringTest;
 public class ReverSTring {
 public static void main(String[] args) {
 	String str="Sachin";
+	System.out.println(revStr(str));
 }
 
  static String revStr(String str1)
 {
-	return "";
+	if(str1.isBlank())
+	{
+		return str1;
+	}
+	else {
+		 return   str1.charAt(str1.length()-1)
+				 +
+	 revStr(str1.substring(0,str1.length()-1));
+	}
 }
 }
